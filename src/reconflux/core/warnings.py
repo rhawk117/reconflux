2df61a3a89e378ee53d9be91e4df65c6c3edf585
rhawk117 <hawkinsry19@gmail.com>
2026-03-07 14:10:25 -0500

    feat: initial core scaffolding setup

---
+++ src/reconflux/core/warnings.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import warnings as py_warnings
+
+
+class ReconfluxWarning(UserWarning):
+    """The base class for all of the warnings emitted by reconflux"""
+    pass
+
+
+def internal_warning(
+    message: str,
+    *,
+    category: type[ReconfluxWarning] = ReconfluxWarning,
+    stacklevel: int = 2
+) -> None:
+    """
+    Emits a ``ReconfluxWarning`` subclass, this is to ensure
+    all warnings emitted can by the library can easily be turned
+    off by users
+    """
+    py_warnings.warn(
+        message=message,
+        category=category,
+        stacklevel=stacklevel,
+    )
+
+
